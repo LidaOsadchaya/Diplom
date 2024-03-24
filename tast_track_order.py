8e5586d36a229b828910323f24c8c63e797bced9
LidaOsadchaya <161446574+LidaOsadchaya@users.noreply.github.com>
2024-03-24 23:02:54 +0300

    Add files via upload

---
+++ tast_track_order.py
@@ -0,0 +1,13 @@
+#Осадчая Лидия, 14-ая кагорта. Финальный проект. Инженер по тестированию плюс
+import pytest
+import sender_stand_request
+
+
+@pytest.mark.test
+def test_track_order():
+    # function for create an order
+    track_number = sender_stand_request.post_create_order()
+
+    response_of_track_order = sender_stand_request.get_info_of_order(track_number)
+
+    assert response_of_track_order.status_code == 200, "Тест успешно пройден"
